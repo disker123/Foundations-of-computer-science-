9565bae5478a5ca3aa26e246fcfaf55409227d4d
Greg <gregkaplo@outlook.com>
2020-10-25 01:11:55 -0400

    Create task14.py

---
+++ task14.py
@@ -0,0 +1,375 @@
+import math
+
+class alphabet:
+	#list of strings
+	
+	def __init__(self, elements):
+		self.alphabet = elements
+	def __repr__(self):
+		return str(self.alphabet)
+	#print function
+	def output(self):
+		print(self.alphabet)
+	def size(self):
+		return len(self.alphabet)
+	def elements(self, index: int):
+		return self.alphabet[index]
+#alphabet test1
+'''
+alph = [5, 7, "hello", "world"]
+x = alphabet(alph)
+print(x)
+alph2 = [1, 2, "this is the second"]
+x = alphabet(alph2)
+#x.output()
+print(x)
+print(x.size())
+'''
+
+class char:
+	#a thing
+	def __init__(self, thing):
+		self.thing = thing
+
+	def __repr__(self):
+		return self.thing
+
+#character test
+'''
+c = "hello world"
+x = char(c)
+print(x)
+'''
+
+class string:
+	# a list of char
+	def __init__(self, elements):
+		self.string = elements
+	def __repr__(self):
+		return str(self.string)
+	def output(self):
+		print(self.string)
+	def append(self, elem):
+		self.string.append(elem)
+#string test
+'''
+a = char("5")
+b = char("7")
+c = char("hello")
+d = char("world")
+y = [a, b, c, d]
+x = string(y)
+print(x)
+'''
+
+'''def Ngen(alphabet: alpha, int: n):
+	collum = log(n+1, alpha.size())
+	row = n - alpha.size()**collum
+'''
+'''
+class cart_pro:
+	def __init__(self, lst, rep):
+		self.lst,	self.rep = rep
+
+	def __iter__(self):
+		self.pos = 0
+		return self
+	
+	def __next__(self):
+		#if(self.pos >= len(self.lst)**self.rep)
+		#	raise StopIteraion
+		n = self.pos
+		product = []
+		self.pos +=1
+		for i in range(self.rep -1, -1, -1):
+			position = int(n / len(self.lst)**i)
+			product.append(self.lst[position])
+			n %= leng(self.lst)**i
+		return product 
+
+
+
+for j in cart_pro([0,1,2,3],4):
+		print(j)
+'''
+
+def Nthstr(alpha: alphabet, n: int):
+	depth = math.floor(math.log(n+1, alpha.size()))#solve for the collum in the LExi order drawing
+	#print("the depth is: ", depth)
+	n = n - alpha.size()**(depth) + 1#get the index of the elemt
+	n = math.floor(n)
+	#print("and the n is: ", n)
+	
+	#print(n)
+	lexi = []
+	for pos in range(depth-1, -1, -1):
+		lexi.append(alpha.alphabet[math.floor(n / alpha.size()**pos)])
+		n = n % alpha.size()**pos
+	return lexi
+
+'''
+n=12
+alpha = alphabet(['0', '1'])
+for n in range(n):#show all elements to test
+	print(n, "	", end=":")
+	print(Nthstr(alpha, n,))#shows the Nth 
+#	print("flag")
+'''
+
+
+'''dfa = {0:{'0':0, '1':1},
+       1:{'0':2, '1':0},
+       2:{'0':1, '1':2}}
+#first test DFA  
+'''
+
+'''
+dfa = {0:{'0':1, '1':1},
+       1:{'0':1, '1':1}}  
+'''
+def accepts(dfa, start, accepting, st):
+	state = start
+	for i in st:
+		state = dfa[state][i]
+	return state in accepting
+'''
+dfa = {0:{'0':0, '1':1},
+       1:{'0':2, '1':0},
+       2:{'0':1, '1':2}}
+
+
+#print(accepts(dfa, 0, {1}, "01100110001"))
+	
+print(accepts(dfa, 0, {0}, ''))#this DFA accepts the empty string
+print(accepts(dfa, 0, {0}, '1'))
+print(accepts(dfa, 0, {0}, '0'))
+'''
+
+'''
+x = 'a'
+
+x = input("enter a single character ")
+dfa = {0:{x:1},
+       1:{x:2},
+       2:{x:2}}
+
+print(accepts(dfa, 0, {1}, x))#accepts a string of 1 char
+x+=x
+print(x)
+print(accepts(dfa, 0, {1}, x))#fails on 'aa'
+'''
+
+dfa_even = {0:{'0':1, '1':1},
+       1:{'0':0, '1':0}}
+
+dfa_even_binary_number = {0:{'0':0, '1':1},
+       1:{'0':0, '1':1}}
+
+dfa_odd_binary_number = {0:{'1':0, '0':1},
+       1:{'1':0, '0':1}}
+
+dfa_name = {0:{'g':1},
+       1:{'r':1, 'e':1, 'g':1}}
+       
+dfa_abst = {0:{'0':0, '1':1},
+       1:{'0':2, '1':1},
+       2:{'0':1, '1':1}}
+
+dfa_element_to_state = {0:{'0':0, '1':1},
+       1:{'0':0, '1':1}}
+
+dfa_three_states = {0:{'0':0, '1':1},
+       1:{'0':2, '1':0},
+       2:{'0':1, '1':2}}
+
+dfa_traffic_light = {0:{'0':0, '1':1},#green
+       1:{'0':2, '1':2},#yellow
+       2:{'0':0, '1':2}}#red
+
+dfa_thermostat = {0:{'1':1, '0':0},
+       1:{'1':1, '0':0}}
+
+dfa_001 = {0:{'1':0, '0':1},
+       1:{'0':2, '1':0},
+       2:{'0':2, '1':3},
+       3:{'0':3, '1':3}}
+
+
+'''
+print(accepts(dfa_even, 0, {0}, "00000000"))#even lengthed dfa  #1
+print(accepts(dfa_even_binary_number, 0, {0}, "11010"))#even number dfa #2
+print(accepts(dfa_odd_binary_number, 1, {0}, "110101"))#odd number dfa #3
+print(accepts(dfa_name, 0, {1}, "greg"))#my name #4
+print(accepts(dfa_abst, 0, {1}, "0010001"))#abstract dfa from sec 1.4 #5
+print(accepts(dfa_element_to_state, 0, {1}, "0001101"))#goes to the state of the element 0->0 1->1 #6
+print(accepts(dfa_element_to_state, 0, {1}, "0001101"))#three states #7
+print(accepts(dfa_traffic_light, 0, {0}, "00001110000"))#stays green when no cars are waiting then turns yellow for a tick then stays red while there are cars waiting and turns green when there are none #8
+print(accepts(dfa_thermostat, 0, {1}, "0011001"))#thermostat on when hot (1) and off when not (0) goal is to be on #9
+print(accepts(dfa_001, 0, {3}, "0110010101"))#accepts if there is 001 in the string #10
+'''
+
+
+
+
+
+#print("This marks the start of checkpoint 2 task 11")
+#task11
+
+def trace(dfa, start, accepting, st):
+	tracelist = [start]
+	state = start
+	for i in st:
+		state = dfa[state][i]
+		tracelist.append(state);
+	print(tracelist)
+	return state in accepting
+'''
+trace(dfa_even, 0, {0}, "00111000")
+trace(dfa_abst, 0, {1}, "0010001")
+trace(dfa_traffic_light, 0, {0}, "00001110000" )
+'''
+
+#task 12
+'''
+
+def find_accepting(dfa, start, accepting, alph, path):
+	visited = set()
+	state = start
+	if state in accepting:
+		print("found")
+		return path
+	for i in alph:
+		if state not in visited:
+			path.add(i)
+
+	find_accepting(dfa, dfa[state][i], accepting, alph)
+	state = dfa[state][i]
+
+
+
+	print("not found)")
+	return false
+'''
+
+
+def find_accepting(dfa, start, accepting, alph):
+	path = ""
+	state_list = []
+	state = start
+	state_list.append(state)
+	if state in accepting:
+		return path
+	while state not in accepting and state_list != list(dfa):
+		#first move
+		if dfa[state][alph[0]] in state_list:
+			state = dfa[state][alph[1]]
+			#print("flaging at the first element for adding 1")
+			path = path + alph[1]
+			state_list.append(state)
+		else:
+			state = dfa[state][alph[0]]
+			#print("flaging at the first element for adding 0")
+			path = path + alph[0] + ""
+			state_list.append(state)
+		
+		if state in accepting:
+			#print(state_list)
+			return path
+		if(len(state_list) > pow(len(dfa), 2)):
+			break
+
+
+	print(state_list)
+	print("failed")
+
+
+
+dfa_num = {0:{'0':3, '1':1},
+       1:{'0':0, '1':2},
+       2:{'0':1, '1':2},
+       3:{'0':0, '1':0}}
+
+dfa_al = {0:{'a':3, 'b':1},
+      	1:{'a':0, 'b':2},
+      	2:{'a':1, 'b':2},
+       	3:{'a':0, 'b':0}}
+
+dfa_large = {0:{'0':3, '1':1},
+       1:{'0':4, '1':2},
+       2:{'0':6, '1':4},
+       3:{'0':2, '1':0},
+       4:{'0':3, '1':7},
+       5:{'0':4, '1':7},
+       6:{'0':7, '1':0},
+       7:{'0':0, '1':0}}
+
+dfa_large_failure = {0:{'0':3, '1':1},
+       1:{'0':5, '1':2},
+       2:{'0':6, '1':7},
+       3:{'0':2, '1':0},
+       4:{'0':3, '1':7},
+       5:{'0':3, '1':7},
+       6:{'0':7, '1':0},
+       7:{'0':0, '1':0}}
+'''
+print(find_accepting(dfa_num, 0, {2}, ['0', '1']));
+print(find_accepting(dfa_al, 0, {2}, ['a', 'b']));
+print(find_accepting(dfa_large, 0, {4}, ['0', '1']));
+print(find_accepting(dfa_large_failure, 0, {4}, ['0', '1']));
+'''
+#print(len(dfa_num))
+
+#task 13
+'''
+print (dfa_num)
+print(dfa_num[0])
+print(dfa_num[0]["0"])
+temp = dfa_num[0]["0"]
+print(temp)
+dfa_num[0]["0"] = dfa_num[0]["1"]
+dfa_num[0]["1"] = temp
+print(dfa_num[0])
+'''
+
+
+def new_dfa(dfa, alph):
+	print ("given dfa", dfa)
+	#print(len(dfa))
+	for i in range(0, len(dfa)):		
+		#print("flag", i)
+		temp = dfa[i][alph[0]]		
+		dfa[i][alph[0]] = dfa[i][alph[1]]
+		dfa[i][alph[1]] = temp
+		#print(dfa[i])
+	return dfa
+'''
+comp = new_dfa(dfa_num, ['0', '1'])
+print("comp  dfa",comp)
+'''
+
+
+#task 14
+#print(dfa_num)
+#print(dfa_001)
+
+def union_accept(dfa, start, st):
+		state = start
+		for i in st:
+			state = dfa[state][i]
+		#print(state)
+		return state 
+
+def union(dfa_a, dfa_b, start_a, start_b, acc_a, acc_b, alph):#(dfa, dfa, 0, 0, {1}, {2}, ['0', '1'])
+	x = find_accepting(dfa_a, start_a, acc_a, alph)#0111
+	y = find_accepting(dfa_b, start_b, acc_b, alph)#001
+	dfa_u = {**dfa_num, **dfa_001}
+	#print(dfa_u)
+	a =union_accept(dfa_u, 0, x)
+	b =union_accept(dfa_u, 0, y)
+	accepting_list = {a, b}
+	print(accepting_list)
+	print(dfa_u)
+	return dfa_u
+
+
+union(dfa_num, dfa_001, 0, 0, {2}, {3}, ['0', '1'])
