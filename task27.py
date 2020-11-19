26b46b2bb03af2294b508e0f31b4730e8607d9e7
Greg <gregkaplo@outlook.com>
2020-11-19 09:35:07 -0500

    Update task27.py

--- task27.py
+++ task27.py
@@ -13,18 +13,23 @@ class NFA:
 
 def oracle(nfa, trace, string, accept):
 	state = nfa.start_state[0]
-	count = 1
+	count = 0
 	#string = "00011"
 	for i in string:
 		#print(state)
 		if(i in nfa.dict[state]):
-			print(state)
+			#print(state)
 			#print("flag")
+			#print(count)
+			print(state)
+			print("-", trace[count])
 			state = nfa.dict[trace[count]][i]
 			#print(state)
 			state = int(state[0])
 			#print(state)
 			count+= 1
+		else:
+			accept = False
 		#print("after state change")
 		#print(state)
 		#state = (int(state))
@@ -32,17 +37,17 @@ def oracle(nfa, trace, string, accept):
 		print("good")
 		return state in nfa.accepting_state
 	if(not accept):
-		print("bad")
-		return state not in nfa.accepting_state
+		print("not accepted")
+		return False+
 
 
 
 
 
 dic_oracle = {1:{'0':['2']},
-         2:{'0':['2'], '1':['2']}}
-trace_5 = [1, 2, 2, 2, 2, 2, 2] #"0" 
+         2:{'0':['2'], '1':['1']}}
+trace_5 = [1, 2, 2, 1, 2, 1, 2] #"0" 
 nfa = NFA([1, 2], ['0', '1'], dic_oracle, [1], [2])
 
-oracle(nfa, trace_5, "001010", True)
+oracle(nfa, trace_5, "001010", [2])
 
