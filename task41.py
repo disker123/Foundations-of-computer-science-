8cb2e28d88a2658ead8a596bd92e9f37f90cbd4e
Greg <gregkaplo@outlook.com>
2020-12-07 17:21:28 -0500

    Create task41.py

---
+++ task41.py
@@ -0,0 +1,10 @@
+#task41 regular expression
+
+class regex:
+	def __init__(self, e, empty, char, union, star, concat):
+		self.e = e
+		self.empty = empty
+		self.char = char
+		self.union = union
+		self.star = star
+		self.concat = concat
