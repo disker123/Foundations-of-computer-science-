dc53b95fd00b33b344858c6033024225461945d7
Greg <gregkaplo@outlook.com>
2020-12-07 20:50:08 -0500

    Update task41.py

--- task41.py
+++ task41.py
@@ -1,10 +1,11 @@
-#task41 regular expression
 
 class regex:
-	def __init__(self, e, empty, char, union, star, concat):
-		self.e = e
+	def __init__(self, language):
+		'''self.e = e
 		self.empty = empty
 		self.char = char
 		self.union = union
 		self.star = star
-		self.concat = concat
+		self.concat = concat'''
+		self.pattern = re.compile(language)
+		return
