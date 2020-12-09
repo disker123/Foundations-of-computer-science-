45714a8035ab74e8a5292f38cbc647ac4850800d
Greg <gregkaplo@outlook.com>
2020-12-08 20:05:26 -0500

    Update task42.py

--- task42.py
+++ task42.py
@@ -1,30 +1,38 @@
-#task41 regular expression
 
 class regex:
-	def __init__(self, language):
+	def __init__(self, char=None, epsilon=None, empty=None, union=None, star=None, circ=None):
+		if char != None:
+			self.char = char
+			self.state = 0
+		elif(epsilon != None):
+			self.epsilon = epsilon
+			self.state = 1
+		elif(empty != None):
+			self.empty = empty
+			self.state = 2
+		elif(union != None):
+			self.union = union
+			self.state = 3
+		elif(star != None):
+			self.star = star
+			self.state = 4
+		elif(circ != None):
+			self.circ = circ
+			self.state = 5
 
-		self.pattern = re.compile(language)
-		return
-
-#task42 regex print
-
-def re_print(reg, string):
-	pattern = reg.pattern
-	#result = pattern.search(string)
-	result = pattern.match(string)
-	if result:
-		print(result.group())
-		return result.group()
-	print("none flag")
-	return None
-
-# Call our function, passing in our string
-
-min1 = regex(r"0*10*")#contains a one
-x = re_print(min1, "00100 james")
-many1 = regex(r"^1")#starts with 1
-x = re_print(many1, "1010")
-many1 = regex(r".1*$")#ends with 1
-x = re_print(many1, "011")
-many1 = regex(r"^0.1*$")#ends with 1 starts with 0
-x = re_print(many1, "011")
+#task 42
+	def printer(self):
+		if self.state == 0:
+			return self.char#return an established chacrackter nfa 
+		if self.state == 1:
+			#print("epsilon")
+			return "epsilon"#epsilon is an established nfa 
+		if self.state == 2:
+			#print("empty")
+			return "empty"#and empty nfa is an established nfa 
+		if self.state == 3:
+			return f"({self.char[0].printer()}) | ({self.char[1].printer()})"# take the union of all the nfas so far in the tree
+		if self.state == 4:
+			return f"({self.star.printer()})*"#useing the kleen star function all the nfas in the tree 
+		if self.state == 5:
+			return f"({self.circ[0].printer()})({self.circ[1].printer()})"
