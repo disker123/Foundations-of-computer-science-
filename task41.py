cedb47a4f9360c7f1b93ffea976fbe46a3787c1b
Greg <gregkaplo@outlook.com>
2020-12-08 20:04:57 -0500

    Update task41.py

--- task41.py
+++ task41.py
@@ -1,11 +1,23 @@
 
+#task41 regular expression
+
 class regex:
-	def __init__(self, language):
-		'''self.e = e
-		self.empty = empty
-		self.char = char
-		self.union = union
-		self.star = star
-		self.concat = concat'''
-		self.pattern = re.compile(language)
-		return
+	def __init__(self, char=None, epsilon=None, empty=None, union=None, star=None, circ=None):
+		if char != None:
+			self.char = char
+			self.state = 0
+		elif(epsilon != None):
+			self.epsilon = epsilon
+			self.state = 1
+		elif(empty != None):
+			self.empty = empty
+			self.state = 2
+		elif(union != None):
+			self.union = union
+			self.state = 3
+		elif(star != None):
+			self.star = star
+			self.state = 4
+		elif(circ != None):
+			self.circ = circ
+			self.state = 5
